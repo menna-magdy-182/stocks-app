139b5c3017ef6714198c340129cfe48911f42c40
menna magdy <menna.magdy@nfq.com>
2024-11-09 23:20:06 +0200

    chore: configure error boundary

--- App.tsx
+++ App.tsx
@@ -5,21 +5,24 @@
  * @format
  */
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
-import {StatusBar} from 'components';
+import {ErrorFallback, StatusBar} from 'components';
 import AppNavigator from 'navigation/AppNavigation';
 import React from 'react';
+import ErrorBoundary from 'react-native-error-boundary';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 function App(): React.JSX.Element {
   const queryClient = new QueryClient();
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <SafeAreaProvider>
-        <StatusBar />
-        <AppNavigator />
-      </SafeAreaProvider>
-    </QueryClientProvider>
+    <ErrorBoundary FallbackComponent={ErrorFallback}>
+      <QueryClientProvider client={queryClient}>
+        <SafeAreaProvider>
+          <StatusBar />
+          <AppNavigator />
+        </SafeAreaProvider>
+      </QueryClientProvider>
+    </ErrorBoundary>
   );
 }
 
